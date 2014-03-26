e09971602f461a5278344db33aa3fed7e28ebbe9
Fidelis Chimombe <fidelisc@mit.edu>
2014-03-26 15:00:44 -0400

    i will continue withthe stuff, let me work ok on 6.005 sockets

--- gen/com/example/auvijo/R.java
+++ gen/com/example/auvijo/R.java
@@ -23,23 +23,31 @@ public final class R {
     public static final class drawable {
         public static final int audioicon=0x7f020000;
         public static final int auvijo=0x7f020001;
-        public static final int exiticon=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int myjournal=0x7f020004;
-        public static final int texticon=0x7f020005;
-        public static final int videoicon=0x7f020006;
+        public static final int delete=0x7f020002;
+        public static final int exiticon=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int myjournal=0x7f020005;
+        public static final int saveicon=0x7f020006;
+        public static final int startbutton=0x7f020007;
+        public static final int stopicon=0x7f020008;
+        public static final int texticon=0x7f020009;
+        public static final int videoicon=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int activityAudio=0x7f080005;
-        public static final int experimental=0x7f080008;
+        public static final int action_settings=0x7f08000d;
+        public static final int camera_preview=0x7f08000c;
+        public static final int deleteButton=0x7f080007;
+        public static final int experimental=0x7f08000b;
         public static final int mainAudio=0x7f080000;
         public static final int mainExit=0x7f080004;
         public static final int mainJournal=0x7f080003;
         public static final int mainText=0x7f080002;
         public static final int mainVideo=0x7f080001;
-        public static final int saveText=0x7f080007;
-        public static final int writtenTextEntry=0x7f080006;
+        public static final int playButton=0x7f080008;
+        public static final int saveButton=0x7f080005;
+        public static final int saveText=0x7f08000a;
+        public static final int startButton=0x7f080006;
+        public static final int writtenTextEntry=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/auvijo/Audio.java
+++ src/com/example/auvijo/Audio.java
@@ -1,24 +1,306 @@
 package com.example.auvijo;
 
+/*
+ * The application needs to have the permission to write to external storage
+ * if the output file is written to the external storage, and also the
+ * permission to record audio. These permissions must be set in the
+ * application's AndroidManifest.xml file, with something like:
+ *
+ * <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+ * <uses-permission android:name="android.permission.RECORD_AUDIO" />
+ *
+ */
+
 import android.app.Activity;
+import android.widget.LinearLayout;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.Gravity;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.content.Context;
+import android.util.Log;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
 
-public class Audio extends Activity implements View.OnClickListener {
-	
+import java.io.IOException;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.audio);
-	}
 
-	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+public class Audio extends Activity
+{
+    private static final String LOG_TAG = "Audio";
+    private static String mFileName = null;
+    Button startButt,saveButt,deleteButt,playButt;
 
+    private RecordButton mRecordButton = null;
+    private MediaRecorder mRecorder = null;
+
+    private PlayButton   mPlayButton = null;
+    private MediaPlayer   mPlayer = null;
+    private SaveButton mSaveButton=null;
+   
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    class RecordButton extends Button {
+        boolean mStartRecording = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {                	
+                	setBackgroundResource(R.drawable.stopicon);
+                    //setText("Stop recording");
+                } else {
+                	setBackgroundResource(R.drawable.startbutton);
+                    //setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        };
+
+        public RecordButton(Context ctx) {
+            super(ctx);
+            setBackgroundResource(R.drawable.startbutton);
+            setOnClickListener(clicker);
+        }
+    }
+
+    class PlayButton extends Button {
+        boolean mStartPlaying = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    setText("Stop playing");
+                } else {
+                    setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        };
+
+        public PlayButton(Context ctx) {
+            super(ctx);
+            setText("Start playing");
+            setOnClickListener(clicker);
+            
+        	
+
+            
+            
+        }
+    }
+   
+    
+    class SaveButton extends Button {
+        boolean mStartPlaying = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    setText("Stop playing");
+                } else {
+                    setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        };
+
+        public SaveButton(Context ctx) {
+            super(ctx);
+            setText("SAVE");
+            setOnClickListener(clicker);
+            
+        	
+
+            
+            
+        }
+    }
+    
+//public void onClick(View v){
+//	boolean mStartPlaying=true;
+//	switch(v.getId()){
+//	case R.id.startButton:
+//		onRecord(mStartPlaying);
+//		if(mStartPlaying){
+//			Log.d("fide-butt",String.valueOf(startButt==null));
+//			startButt.setBackgroundResource(R.drawable.stopicon);			
+//			
+//		
+//		}else{
+//			startButt.setBackgroundResource(R.drawable.startbutton);
+//			playButt.setVisibility(RESULT_OK);
+//		}
+//		mStartPlaying = !mStartPlaying;
+//		break;
+//	case R.id.saveButton:
+//		break;
+//	case R.id.deleteButton:
+//		break;
+//	case R.id.playButton:
+//		onPlay(mStartPlaying);
+//		if(mStartPlaying){
+//			startButt.setBackgroundResource(R.drawable.stopicon);			
+//		}else{
+//			startButt.setBackgroundResource(R.drawable.audioicon);
+//			
+//		}
+//		break;
+//	}
+//}
+    public Audio() {
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+    }
+public void initialize(){
+	startButt=(Button)findViewById(R.id.startButton);
+    saveButt=(Button)findViewById(R.id.saveButton);
+    deleteButt=(Button)findViewById(R.id.deleteButton);
+    playButt=(Button)findViewById(R.id.playButton);
+}
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        LinearLayout ll = new LinearLayout(this);
+        mRecordButton = new RecordButton(this);
+        ll.setWeightSum(100);
+        
+        ll.addView(mRecordButton,
+            new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                0));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            
+            
+            params.gravity=Gravity.BOTTOM;
+            params.height=100;
+            params.width=100;
+            params.weight=40;
+            mRecordButton.setLayoutParams(params);
+//        ll.addView(mRecordButton,
+//                new LinearLayout.LayoutParams(
+//                    ViewGroup.LayoutParams.WRAP_CONTENT,
+//                    ViewGroup.LayoutParams.WRAP_CONTENT,
+//                    0));
+       
+        
+        
+        
+        
+        
+        mPlayButton = new PlayButton(this);
+        ll.addView(mPlayButton,
+            new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                0));
+        LinearLayout.LayoutParams paramss= new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            
+            paramss.gravity=Gravity.BOTTOM;
+            paramss.height=100;
+            paramss.weight=40;
+            paramss.width=100;
+            
+            
+
+            mPlayButton.setLayoutParams(paramss);
+         
+            
+            mSaveButton = new SaveButton(this);
+            ll.addView(mSaveButton,
+                new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    0));
+            LinearLayout.LayoutParams para= new LinearLayout.LayoutParams(
+                    LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                
+                para.gravity=Gravity.BOTTOM;
+                paramss.height=100;
+                paramss.weight=20;
+                paramss.width=100;
+                
+                
+
+                mSaveButton.setLayoutParams(paramss);
+        
+        setContentView(ll);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
 }
